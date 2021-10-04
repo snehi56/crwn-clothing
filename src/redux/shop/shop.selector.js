c3914cec425713b872a7ba84e3f140079337c7f0
Snehal S Joshi <Snehal12083@Mastek.com>
2021-10-04 11:42:05 +0530

    shop selector modified

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -5,10 +5,16 @@ const selectShop = state => state.shop;
 export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
-);
-
-export const selectCollection  = collectionUrlParam =>
+  );
+  
+  export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+  );
+  
+  export const selectCollection = collectionUrlParam =>
     createSelector(
-        [selectCollections],
-        collections => collections[collectionUrlParam]
+      [selectCollections],
+      collections => collections[collectionUrlParam]
     );
+  
